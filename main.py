59a30d84d74e0a6eabcb3ded14c24e306a61831e
careatomkarenterprises-ops <232618332+careatomkarenterprises-ops@users.noreply.github.com>
2026-04-21 16:53:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from src.scanner.master_scanner import MasterScanner
 if __name__ == "__main__":
     scanner = MasterScanner()
     scanner.scan_intraday_fno()
-    scanner.scan_currency()
     scanner.scan_commodity()
     scanner.post_delayed_patterns()
